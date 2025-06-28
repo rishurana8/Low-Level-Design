a878267c94f5d5441d56c178f64449f895dbe9ad
rishurana8 <rishurana256@gmail.com>
2025-06-29 03:32:13 +0530

    some changes for strategy design pattern

--- Design Patterns/src/main/java/org/rishudesign/com/designpatterns/Strategydesignpattern/withstrategypattern/Main.java
+++ Design Patterns/src/main/java/org/rishudesign/com/designpatterns/Strategydesignpattern/withstrategypattern/Main.java
@@ -1,15 +1,23 @@
 package org.rishudesign.com.designpatterns.Strategydesignpattern.withstrategypattern;
 
+import org.rishudesign.com.designpatterns.Strategydesignpattern.withstrategypattern.Strategy.NormalDriveStrategy;
+
 public class Main {
 
     public static void main(String args[]) {
 
-        Vechile vechile = new SportsVechile();
+        Vechile vechile = new OffRoadVechile();
         vechile.drive();
 
-        System.out.println("\nAnother Testing\n");
+        System.out.println("\n");
 
         Vechile vechile2 = new GoodsVechile();
-        vechile.drive();
+        vechile2.drive();
+
+        System.out.println("\n");
+        // here we are creating object of normal vechile and here we have to manually pass in constructor drive strategy
+        // As here we are not creating any object of sportsvechile goodsvechile direclty
+        Vechile vechile3 = new Vechile(new NormalDriveStrategy());
+        vechile3.drive();
     }
 }
